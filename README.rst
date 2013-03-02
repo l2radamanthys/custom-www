fc90a6af7b8d3916c30f3d08ec058d1b4365bc58
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2013-03-02 01:17:52 -0200

    Agregada la primera version, completamente funcional y la documentacion de instalacion

---
+++ README.rst
@@ -0,0 +1,24 @@
+Custom-WWW
+==========
+    Sencilla Pagina Para listar los proyectos (carpetas) de su directorio de desarrollo
+    personalizado en LAMP o WAMP, tambien se puede usar en directorios virtuales.
+
+
+Autor
+-----
+    Ricardo D. Quiroga - l2radamanthys@gmail.com
+    _Sitio Web: http://wwww.co2soft.com.ar/
+    
+
+Instalacion
+-----------
+    La instalacion es bastante sencilla, solo tiene que copiar los siguientes archivos
+    (reemplazar si es necesario) en su directorio raiz de desarollo:
+
+    _data
+    _styles
+    _images
+    index.php
+
+    **Nota:** El directorio raiz suele estar ubicado en linux en "/var/www".
+
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+$FOLDER_ICON = '_images/folder.png';
+$DOES_NOT_SHOW = array(
+	".", 
+	"..", 
+	"_images", #carpeta de iconos
+	"_styles", #carpeta de stilos
+	"_data", #otros
+
+	#archivos o carpetas escluidos personalizados
+	".metadata",
+);
+
+echo file_get_contents('_data/templates/header.html');
+
+$PATH =  getcwd();
+$FILE_LIST = opendir($PATH);
+
+while($file_name = readdir($FILE_LIST)) {
+	if (in_array($file_name, $DOES_NOT_SHOW)) {
+		continue;
+	}
+	$file_path = $PATH."/".$file_name;
+	#Unix
+	/*
+	if (is_dir($file_path)) {
+		echo '<li><img src="'.$FOLDER_ICON.'" align="center"><a href="'.$file_name.'">'.$file_name.'</a></li>';
+	}
+    
+    else {
+		echo "<li> Error: ".$file_name."</li>";
+    }
+    */
+    #Windows
+	if (is_dir($file_name)) {   
+		echo '<li><img src="'.$FOLDER_ICON.'" align="center"><a href="'.$file_name.'">'.$file_name.'</a></li>';
+	}
+    
+    else {
+		#solo para debug
+		#echo "<li> Error: ".$file_name."</li>";
+    }
+
+}
+
+echo file_get_contents('_data/templates/footer.html');
+?>
